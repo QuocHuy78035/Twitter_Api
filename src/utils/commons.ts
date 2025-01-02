c141163d2c29ac3cacbfd12bfde2c5665e7dae98
quochuywork <huypro0320092003@gmail.com>
2025-01-02 15:35:11 +0700

    chore: updated dev2

--- src/utils/commons.ts
+++ src/utils/commons.ts
@@ -1,3 +1,4 @@
 export const numberEnumToArr = (numberEnum: { [key: string]: string | number }) => {
   return Object.values(numberEnum).filter((value) => typeof value == 'number') as number[]
 }
+///this is for dev2
